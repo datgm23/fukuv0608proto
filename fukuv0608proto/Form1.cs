7a8daf058127f816721df6b7d4d5cf0ee754996f
YuTanaka <tanakaedu@users.noreply.github.com>
2023-06-08 13:23:09 +0900

    完成

--- fukuv0608proto/Form1.cs
+++ fukuv0608proto/Form1.cs
@@ -2,9 +2,46 @@ namespace fukuv0608proto
 {
     public partial class Form1 : Form
     {
+        int vx = -5;
+        int vy = -5;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
+
+            if (label1.Left < 0)
+            {
+                vx = Math.Abs(vx);
+            }
+            else if (label1.Right > ClientSize.Width)
+            {
+                vx = -Math.Abs(vx);
+            }
+
+            if (label1.Top < 0)
+            {
+                vy = Math.Abs(vx);
+            }
+            else if (label1.Bottom > ClientSize.Height)
+            {
+                vy = -Math.Abs(vy);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            label1.Text = "Tanaka Yu";
         }
     }
 }
